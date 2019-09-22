c4c1e62b95f15e097e3631e1f4595cbadcf345ee
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-22 20:18:31 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -5,92 +5,110 @@
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# def introduction(name)
-#  puts "Hi, my name is #{name}."
+#def introduction(name)
+  #puts "Hi, my name is #{name}."
 #end
 
-#def introduction_with_language(name, language)
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+#def introduction_with_language(name,language)
+ #puts "Hi my name is #{name}, #{language}"
 #end
+ 
+#introduction_with_language
 
-#def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
+#def introduction_with_language_optional(name, language= 
+#'ruby")
+#   puts "Hi my name is #{name} and I am learning to program #{language}"
+# end
+   
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def introduction(name = "Bri")
+ puts "Hi, my name is #{name}."
+end
+
+introduction ("bri")
+
+def introduction_with_language(name, language)
+ puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+introduction_with_language("Bri", "Ruby")
+
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
+
+introduction_with_language_optional("Bri")
