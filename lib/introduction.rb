2496d992e75e0cb8e6c47fba667a260e0e674c62
Brianna Fetterley <bnfetterley@usfca.edu>
2019-09-22 20:15:42 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -107,7 +107,7 @@ def introduction_with_language(name, language)
 end
 introduction_with_language("Bri", "Ruby")
 
-def introduction_with_language_optional(name = "Bri", language="Ruby")
+def introduction_with_language_optional(name, language="Ruby")
   puts "Hi, my name is #{name} and I am learning to program in #{language}."
 end
 
